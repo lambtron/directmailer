7de8405eede9505172a99e26c1e498219bb2c03c
Andy Jiang <andyjiang@gmail.com>
2013-12-27 09:27:42 -0800

    fixed LOB wrapper library helper

--- app/helpers/lob.js
+++ app/helpers/lob.js
@@ -2,6 +2,7 @@ use 'strict'
 
 // Set the Lob API credentials.
 var LOB = new (require('lob')) (process.env.LOB_API_KEY);
+var _ = require('underscore');
 
 // Public functions.
 module.exports = {
@@ -12,18 +13,10 @@ module.exports = {
 	// - quantity (optional, defaults to 1)
 	// - double sided (optional)
 	createObject: function(obj, cb) {
-		// if key or value is undefined, then set it to default.
-		// copy object but if undefined take default values?
-		var name = (typeof n === "undefined") ? "" : n;
-		var quantity = (typeof q === "undefined") ? 1 : q;
-		var double_sided = (typeof ds === "undefined") ? true : ds;
-		LOB.objects.create({
-			name: name,
-			file: filePath,
-			setting_id: setting_id,
-			quantity: quantity,
-			double_sided: double_sided
-		}, function(err, res) {
+		obj.name = (typeof obj.name === "undefined") ? "" : obj.name;
+		obj.quantity = (typeof obj.quantity === "undefined") ? 1 : obj.quantity;
+		obj.double_sided = (typeof obj.double_sided === "undefined") ? true : obj.double_sided;
+		LOB.objects.create(obj, function(err, res) {
 			cb(err, res);
 		});
 	},
@@ -39,20 +32,15 @@ module.exports = {
 	// - address_country (req)
 	// TODO: pass an obj with the parameters as key:value pairs.
 	createAddress: function(addressObj, cb) {
-		var email = (typeof e === "undefined") ? "" : e;
-		var phone = (typeof p === "undefined") ? "" : p;
-		var address_line2 = (typeof add_line2 === "undefined") ? "" : add_line2;
-		LOB.addresses.create({
-			name: name,
-			email: email,
-			phone: phone,
-			address_line1: address_line1,
-			address_line2: address_line2,
-			address_city: address_city,
-			address_state: address_state,
-			address_zip: address_zip,
-			address_country: address_country,
-		}, function(err, res) {
+		var keys = _.keys(addressObj);
+		var totalKeys = ["name", "email", "phone", "address_line1", "address_line2", "address_city", 
+			"address_state", "address_zip", "address_country"];
+		var missingKeys = _.difference(keys, totalKeys);
+		// Set default values for the missing keys.
+		for (var i = 0; i < missingKeys.length; i ++) {
+			addressObj[missingKeys[i]] = "";
+		}
+		LOB.addresses.create(addressObj, function(err, res) {
 			cb(err, res);
 		});
 	},
@@ -60,17 +48,15 @@ module.exports = {
 	// - all arguments are optional here, except cb)
 	// TODO: pass an obj with the parameters as key:value pairs.
 	verifyAddress: function(addressObj, cb) {
-		for (var i = 0; i < arguments.length; i++) {
-			arguments[i] = (typeof arguments[i] === "undefined") ? "" : arguments[i];
-		};
-		LOB.addresses.verify({
-			address_line1: address_line1,
-			address_line2: address_line2,
-			address_city: address_city,
-			address_state: address_state,
-			address_zip: address_zip,
-			address_country: address_country
-		}, function(err, res) {
+		var keys = _.keys(addressObj);
+		var totalKeys = ["address_line1", "address_line2", "address_city", "address_state",
+			"address_zip", "address_country"];
+		var missingKeys = _.difference(keys, totalKeys);
+		// Set default values for the missing keys.
+		for (var i = 0; i < missingKeys.length; i ++) {
+			addressObj[missingKeys[i]] = "";
+		}
+		LOB.addresses.verify(addressObj, function(err, res) {
 			cb(err, res);
 		});
 	},
@@ -81,22 +67,14 @@ module.exports = {
 	// - cb (req)
 	// - rest are opt
 	sendJob: function(jobObj, cb) {
-		for (var i = 0; i < arguments.length; i++) {
-			arguments[i] = (typeof arguments[i] === "undefined") ? "" : arguments[i];
-		};
-		if ( cb === "" ) {
-			cb = function(err, res) {
-				console.log(err, res);
-			};
-		};
-		LOB.jobs.create({
-			name: name,
-			from: from,
-			to: to,
-			object1: object
-			// packaging_id: packaging_id,
-			// service_id: service_id
-		}, function(err, res) {
+		var keys = _.keys(jobObj);
+		var totalKeys = ["to", "from", "object1", "name"];
+		var missingKeys = _.difference(keys, totalKeys);
+		// Set default values for the missing keys.
+		for (var i = 0; i < missingKeys.length; i ++) {
+			jobObj[missingKeys[i]] = "";
+		}
+		LOB.jobs.create(jobObj, function(err, res) {
 			cb(err, res);
 		});
 	}
